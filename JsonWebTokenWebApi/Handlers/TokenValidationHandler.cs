5f39ecdca03be469d70d2a9c8d65842916e29fd0
pauln <temetra.xy+git@gmail.com>
2018-11-30 17:45:07 +0000

    Returning Unauthorized for some exceptions

--- JsonWebTokenWebApi/Handlers/TokenValidationHandler.cs
+++ JsonWebTokenWebApi/Handlers/TokenValidationHandler.cs
@@ -1,9 +1,12 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using JsonWebTokenWebApi.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JsonWebTokenWebApi.Handlers
 {
@@ -11,19 +14,37 @@ namespace JsonWebTokenWebApi.Handlers
 	{
 		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
 		{
-			// If a token and cookie are provided an attempt to authorize is made
-			// Throws an exception if attempt fails
-			ClaimsPrincipal principle = AuthorizationHelper.Instance.ProcessAuthorizationRequest(request);
+			HttpResponseMessage message = null;
 
-			// Set Principle, which is used by subsequent authorization filters
-			if (principle != null)
+			try
 			{
-				Thread.CurrentPrincipal = principle;
-				HttpContext.Current.User = principle;
+				// If a token and cookie are provided an attempt to authorize is made
+				// Throws an exception if attempt fails
+				ClaimsPrincipal principle = AuthorizationHelper.Instance.ProcessAuthorizationRequest(request);
+
+				// Set Principle, which is used by subsequent authorization filters
+				if (principle != null)
+				{
+					Thread.CurrentPrincipal = principle;
+					HttpContext.Current.User = principle;
+				}
+			}
+			catch (SecurityTokenValidationException secEx)
+			{
+				// An unhandled SecurityTokenValidationException results in HTTP 500
+				message = request.CreateResponse(HttpStatusCode.Unauthorized, new { secEx.Message });
+			}
+			catch (ArgumentException argEx)
+			{
+				// Can happen if authorization header is invalid, results in HTTP 500
+				message = request.CreateResponse(HttpStatusCode.Unauthorized, new { argEx.Message });
 			}
 
 			// Continue handling request
-			return await base.SendAsync(request, cancellationToken);
+			if (message == null) message = await base.SendAsync(request, cancellationToken);
+
+			// Return response
+			return message;
 		}
 	}
 }
